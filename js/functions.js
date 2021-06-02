c6f753b11baa78141e640e1524cd3ae504535730
Maciej Michalik <halniak24@gmail.com>
2021-06-02 12:28:24 +0200

    Add script functions and logs

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
 function printMessage(msg) {
+    console.log('wywołanie funkcji printMessage z argumentem: ', msg);
     let div = document.createElement('div');
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
@@ -6,4 +7,36 @@ function printMessage(msg) {
 
 function clearMessages() {
     document.getElementById('messages').innerHTML = '';
+}
+
+function getMoveName(moveId) {
+    console.log('wywołanie funkcji getMoveName z argumentem: ', moveId);
+    if (moveId == 1) {
+        return 'papier';
+    } else if (moveId == 2) {
+        return 'kamień';
+    } else if (moveId == 3) {
+        return 'nożyczki';
+    }
+}
+
+function displayResult(computerMove, playerMove) {
+    console.log('wywołanie funkcji displayResult z argumentem: ', computerMove, ' i ', playerMove);
+    if (computerMove == playerMove) {
+        return 'remis';
+    } else if (playerMove == 'papier' && computerMove == 'kamień') {
+        return 'wygrana gracza';
+    } else if (playerMove == 'papier' && computerMove == 'nożyczki') {
+        return 'wygrana komputera';
+    } else if (playerMove == 'kamień' && computerMove == 'papier') {
+        return 'wygrana komputera';
+    } else if (playerMove == 'kamień' && computerMove == 'nożyczki') {
+        return 'wygrana gracza.'
+    } else if (playerMove == 'nożyczki' && computerMove == 'papier') {
+        return 'wygrana gracza.'
+    } else if (playerMove == 'nożyczki' && computerMove == 'kamień') {
+        return 'wygrana komputera';
+    } else {
+        return 'gra nierozstrzygnięta';
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,49 +1,16 @@
 // Zagranie komputera
 
 let randomInteger = Math.floor(Math.random() * 3 + 1);
-let computerMove = '';
-
 console.log('Komputer wylosował: ' + randomInteger);
-
-if (randomInteger == 1) {
-    computerMove = 'papier';
-} else if (randomInteger == 2) {
-    computerMove = 'kamień';
-} else if (randomInteger == 3) {
-    computerMove = 'nożyczki';
-}
-
+let computerMove = getMoveName(randomInteger);
+console.log('Ruch komputera to: ' + computerMove);
 // Zagranie gracza
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyczki');
-let playerMove = 'nieznany ruch';
-
 console.log('Gracz wybrał: ' + playerInput);
-
-if (playerInput == 1) {
-    playerMove = 'papier';
-} else if (playerInput == 2) {
-    playerMove = 'kamień';
-} else if (playerInput == 3) {
-    playerMove = 'nożyczki';
-}
+let playerMove = getMoveName(playerInput);
+console.log('Ruch gracza to: ' + playerMove);
 
 // Rozstygnięcie gry
 
-if (computerMove == playerMove) {
-    printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to remis.');
-} else if (playerMove == 'papier' && computerMove == 'kamień') {
-    printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to wygrana gracza.');
-} else if (playerMove == 'papier' && computerMove == 'nożyczki') {
-    printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to wygrana komputera.');
-} else if (playerMove == 'kamień' && computerMove == 'papier') {
-    printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to wygrana komputera.');
-} else if (playerMove == 'kamień' && computerMove == 'nożyczki') {
-    printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to wygrana gracza.');
-} else if (playerMove == 'nożyczki' && computerMove == 'papier') {
-    printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to wygrana gracza.');
-} else if (playerMove == 'nożyczki' && computerMove == 'kamień') {
-    printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to wygrana komputera.');
-} else {
-    printMessage('Gracz wybrał nieprawidłowo. Gra nierozstrzygnięta.');
-}
+printMessage('komputer wylosował ' + computerMove + ', gracz wybrał ' + playerMove + '. Wynik to ' + displayResult(computerMove, playerMove));
