a429a4bc445a7833a580d9499c7d12fca5c7871c
Maciej Michalik <halniak24@gmail.com>
2021-06-02 10:10:51 +0200

    Add preliminary script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-let computerMove = 'kamień';
 let playerMove = 'papier';
 
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+
+let randomInteger = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomInteger);
+
+let computerMove = 'nieznany ruch';
+
+if (randomInteger == 1) {
+    computerMove = 'Papier';
+}
+else if (randomInteger == 2) {
+    computerMove = 'Kamień';
+}
+else if (randomInteger == 3) {
+    computerMove = 'Nożyczki';
+}
+
+printMessage('mój ruch to: ' + computerMove);
